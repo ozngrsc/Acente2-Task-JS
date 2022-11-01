683a19c4690b0d625964fac9c21d3f16b2509b3f
ozngrsc <ozangursucu@hotmail.com>
2022-11-02 01:47:07 +0300

    Acente2-Task-with-JavaScript

---
+++ index.js
@@ -0,0 +1,37 @@
+/* Click Dropdown for Hide and Show Dropdown */
+
+function dropdownFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+/* Outside Click for Close Dropdown */
+
+window.onclick = function (event) {
+  if (!event.target.matches(".dropbtn")) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  }
+};
+
+/* Hamburger Menu */
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-link").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  })
+);
